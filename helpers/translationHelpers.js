77c8a97a9d42be905ccb70bbbc9787c6bc855869
SamrithaS <samritha@timeless.co>
2022-11-17 18:00:55 +0530

    translation helper

--- helpers/translationHelpers.js
+++ helpers/translationHelpers.js
@@ -1,27 +1,11 @@
 import anime from "animejs/lib/anime.es.js";
 
-export function moveBottomSheet(
-  targetBottomSheet,
-  top,
-  ease,
-  duration,
-  animateOnDrag,
-  reset = false
-) {
+export function moveBottomSheet(targetBottomSheet, top, ease, duration) {
   let result = {
     targets: targetBottomSheet,
     translateY: top,
     easing: ease,
     duration,
-    animateOnDrag,
   };
-  animateOnDrag &&
-    Object.keys(animateOnDrag).forEach((i) => {
-      result = { ...result, [i]: animateOnDrag[i] };
-    });
-  if (animateOnDrag && reset)
-    Object.keys(animateOnDrag).forEach((i) => {
-      result = { ...result, [i]: 1 };
-    });
   anime(result);
 }
