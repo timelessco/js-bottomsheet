71825a3419cf9d5a07984f8d366bc6158c174e9e
SamrithaS <samritha@timeless.co>
2022-11-22 18:36:13 +0530

    snappoints change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ Promise.resolve(fetchAllShows()).then(async (res) => {
   document.querySelectorAll(`.scroll-snap-slide`).forEach(async (i, index) => {
     bottomsheet1 = BottomSheet({
       trigger: `target-${index}`,
-      snapPoints: ["30%", "60%", "100%"],
+      snapPoints: ["100%"],
       displayOverlay: false,
       minWidthForModal: 600,
       webLayout: "modal",
