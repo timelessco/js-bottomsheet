ff13300cfcfcca06b44a93d5dd264a1f637f0b17
Navin Moorthy <navin007.a@gmail.com>
2022-12-27 14:56:38 +0530

    build(bundle): 🏗️ repo with better dx

---
+++ vite-lib.config.js
@@ -0,0 +1,45 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+const getPackageName = () => {
+  return "bottomSheet";
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.es.js`,
+  umd: `${getPackageName()}.umd.js`,
+};
+
+export default defineConfig(mode => {
+  return {
+    define: {
+      "process.env.NODE_ENV": `'${mode}'`,
+    },
+    base: "./",
+    build: {
+      outDir: path.resolve(__dirname, "lib"),
+      lib: {
+        entry: path.resolve(__dirname, "src/index.js"),
+        name: getPackageNameCamelCase(),
+        formats: ["es", "umd"],
+        fileName: format => fileName[format],
+      },
+      rollupOptions: {
+        external: ["animejs/lib/anime.es.js"],
+        output: {
+          globals: {
+            "animejs/lib/anime.es.js": "anime",
+          },
+        },
+      },
+    },
+  };
+});
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 // vite.config.js
 import { resolve } from "path";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        maps: resolve(__dirname, "maps.html"),
+export default defineConfig(() => {
+  return {
+    root: resolve(__dirname, "website"),
+    build: {
+      target: browserslistToEsbuild(),
+      outDir: resolve(__dirname, "dist"),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "/index.html"),
+          maps: resolve(__dirname, "/maps.html"),
+        },
       },
     },
-  },
+    plugins: [topLevelAwait()],
+  };
 });
