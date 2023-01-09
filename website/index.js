5757fe7860644be2f5dc05a44b2054c3894730b9
SamrithaS <samritha@timeless.co>
2023-01-09 12:56:19 +0530

    refactor(js): ♻️  linting changes and refactor
    added linting changes and refactored code accordingly
    
    -

---
+++ website/index.js
@@ -0,0 +1,72 @@
+// import BottomSheet from "./lib/bottomsheet.es";
+import BottomSheet from "../src/bottomSheet";
+
+// import BottomSheet from "../lib/bottomSheet.es";
+
+async function fetchAllShows() {
+  const shows = await fetch(
+    "https://strapi.tmls.dev/api/genres?sort[0]=id&fields[0]=name&populate[shows][sort][0]=id&populate[shows][fields][0]=key&populate[shows][fields][1]=name&populate[shows]&populate[shows][populate][poster][fields][0]=hash&populate[shows][populate][poster][fields][1]=src&populate[shows][populate][banner][fields][0]=hash&populate[shows][populate][banner][fields][1]=src",
+  )
+    .then(res => res.json())
+    .then(async json => {
+      const res = json.data[2].shows
+        .concat(json.data[5].shows)
+        .concat(json.data[7].shows);
+      return res;
+    });
+
+  const content = `${shows.map((i, index) => {
+    return `<li id="target-${index}" class="scroll-snap-slide" data-bottomsheet-id = bottomsheet-${index} key = ${i.key}><img src=${i.poster.src}></li>`;
+  })}`;
+  return content.replaceAll(",", "");
+}
+async function getBottomsheet1content(key) {
+  const products = await fetch(
+    `https://strapi.tmls.dev/api/shows?filters[key][$eq]=${key}&populate=%2A`,
+  )
+    .then(res => res.json())
+    .then(async json => {
+      return json.data[0];
+    });
+
+  const content = `
+       <div class="list-items">
+       <div class="img-wrapper">
+       <img src=${products.banner.src}>
+       <div class="gradient"></div>
+       </div>
+       <div class="container">
+       <h1 class="name">${products.name}</h1>
+       <span class="genre">Drama · Comedy · 2021</span>
+       <p class="description">${products.description}</p>
+       <p class="cast">Cast & Crew</p>
+      ${products.cast_and_crew.map(i => `</span>${i.name} </span>`)}
+       <div>
+       <h2>Season 1</h2>
+       ${products.videos.map((item, index) => {
+         return `<div class="container-box">
+         <img src = ${item.poster} />
+         <h4><span class="number">${index + 1}</span> ${item.name}</h4>
+         </div>`;
+       })}
+       </div>
+       </div>
+    </div>
+`;
+  return content.replaceAll(",", "");
+}
+Promise.resolve(fetchAllShows()).then(async res => {
+  document.querySelector(".scroll-snap-slider").innerHTML = res;
+  document.querySelectorAll(`.scroll-snap-slide`).forEach(async (i, index) => {
+    BottomSheet({
+      trigger: `target-${index}`,
+      snapPoints: ["100%"],
+      displayOverlay: false,
+      minWidthForModal: 600,
+      webLayout: "modal",
+      content: `<div id="bottomsheet-${index}" data-bottomsheet> ${await getBottomsheet1content(
+        i.getAttribute("key"),
+      )} </div>`,
+    });
+  });
+});
---
+++ website/maps.js
@@ -0,0 +1,34 @@
+import BottomSheet from "../src/bottomSheet";
+
+BottomSheet({
+  snapPoints: ["20%", "100%"],
+  draggableArea: `<div id="drag">
+  <svg  width="45" height="7" viewBox="0 0 45 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect width="45" height="7" rx="3.5" fill="#38383D"/>
+  </svg>
+  </div>
+  `,
+  dismissible: false,
+  content: ` <div id="maps-1" data-bottomsheet><input class="search" placeholder="Search Maps" /><div class="flex">
+ <p class="fav">Favourites</p> <!-- <p class="more">More</p> --></div><div class="cont-box"> <div class="fav-box" id="map-target" data-bottomsheet-id="maps-2"><figure class="round"><img src="./assets/home.svg" width="30" height="30" /></figure> <p class="sm">Home</p></div><div class="fav-box" id="map-target" data-bottomsheet-id="maps-2">
+<figure class="round"> <img src="./assets/work.svg" width="30" height="30" /></figure> <p class="sm">Work</p></div><div class="fav-box" id="map-target" data-bottomsheet-id="maps-2">
+<figure class="round"><img src="./assets/train.svg" width="30" height="30" /></figure><p class="sm">Transit</p> </div><div class="fav-box" id="map-target" data-bottomsheet-id="maps-2"><figure class="round"><img src="./assets/pin.svg" width="30" height="30" /></figure><p class="sm">6th street</p></div></div><div class="flex"><p class="fav">Recents</p></div><div class="cont-box"><div><div class="flex-start" id="map-target" data-bottomsheet-id="maps-2" ><figure class="round-2"> <img src="./assets/pin.svg" width="20" height="20" /></figure><div><p class="md" >Dropped Pin</p> <p class="xs">Chennai</p></div></div><div class="flex-start" id="map-target" data-bottomsheet-id="maps-2" ><figure class="round-2"><img src="./assets/arrow.svg" width="18" height="18" /></figure><div><p class="md">6th street</p><p class="xs">From my location</p>
+</div> </div></div></div><div class="share"><button class="surround" id="target-map-3" data-bottomsheet-id="maps-3" ><p class="more">Share My Location</p></button><button class="surround" id="target-map-3" data-bottomsheet-id="maps-3"> <p class="more">Mark My Location</p></button><button class="surround" id="target-map-3" data-bottomsheet-id="maps-3"> <p class="more">Report An Issue</p></button><button class="surround" id="target-map-3" data-bottomsheet-id="maps-3"><p class="more">Report An Issue</p></button></div></div>`,
+  openOnLoad: true,
+});
+
+const bottomsheet2 = BottomSheet({
+  trigger: `map-target`,
+});
+document.getElementById("done").addEventListener("click", () => {
+  bottomsheet2.close();
+});
+
+const bottomsheet3 = BottomSheet({
+  trigger: `target-map-3`,
+  displayOverlay: true,
+  snapPoints: ["35%"],
+});
+document.querySelector(".cancel").addEventListener("click", () => {
+  bottomsheet3.close();
+});
