0c5accbf116337cde530f2956d3cea83521d9100
Navin Moorthy <navin007.a@gmail.com>
2023-01-30 13:04:10 +0530

    build(vite): 👷 fix vercel build with vite

--- apps/website/vite.config.js
+++ apps/website/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(() => {
     root: resolve(__dirname, "src"),
     build: {
       target: browserslistToEsbuild(),
+      outDir: resolve(__dirname, "../../dist"),
       rollupOptions: {
         input: {
           main: resolve(__dirname, "src/index.html"),
