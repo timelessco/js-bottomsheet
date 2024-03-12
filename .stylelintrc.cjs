f19ab04f9e30baab61269c7a906e3c7793a35d9c
Navin Moorthy <navin007.a@gmail.com>
2024-03-12 13:25:32 +0530

    refactor(stylelint): 💄 update stylelint configuration and fix CSS styles

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,15 +1,6 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-prettier",
-    "stylelint-config-clean-order",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-clean-order"],
   rules: {
-    // Suppress the following rules as they are already handled by Prettier
-    "value-list-comma-newline-after": null,
-    "declaration-colon-newline-after": null,
-    "max-line-length": 100,
-
     // Add your own rules here
     // Need vendor prefix for the browser support
     "property-no-vendor-prefix": null,
